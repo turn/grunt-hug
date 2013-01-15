e659686bb7c1a04659332b1fc1eb5d6c1a1cbb4d
Ozan Turgut <oturgut@turn.com>
2013-01-15 10:50:49 -0800

    Templating can now be used with config variables
    Signed-off-by: Ozan Turgut <oturgut@turn.com>

--- tasks/hug.js
+++ tasks/hug.js
@@ -6,15 +6,16 @@ module.exports = function(grunt){
 		var complete = this.async(),
 			srcList = grunt.file.expandFiles(this.file.src),
 			options,
+			gruntConfig = grunt.config.get(),
 			destPath = this.file.dest;
 
 		options = {
-			exportedVariable: this.data.exportedVariable,
-			exports: this.data.exports,
+			exportedVariable: this.data.exportedVariable && grunt.template.process(this.data.exportedVariable, gruntConfig),
+			exports: this.data.exports && grunt.template.process(this.data.exports, gruntConfig),
 			headList: this.data.header? grunt.file.expandFiles(this.data.header) : [],
-			moduleVariableName: this.data.moduleVariableName,
-			exportsVariableName: this.data.exportsVariableName,
-			requireFunctionName: this.data.requireFunctionName,
+			moduleVariableName: this.data.moduleVariableName && grunt.template.process(this.data.moduleVariableName, gruntConfig),
+			exportsVariableName: this.data.exportsVariableName && grunt.template.process(this.data.exportsVariableName, gruntConfig),
+			requireFunctionName: this.data.requireFunctionName && grunt.template.process(this.data.requireFunctionName, gruntConfig),
 			success: function(content){
 				grunt.file.write(destPath, content);
 				grunt.log.writeln('File "' + destPath + '" created.');
