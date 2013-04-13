c92515ebff880b78a7925c349624a17a627edd66
Ozan Turgut <ozanturgut@gmail.com>
2013-04-13 10:00:50 -0700

    Grunt 0.4 compatibility
    Signed-off-by: Ozan Turgut <ozanturgut@gmail.com>

--- tasks/hug.js
+++ tasks/hug.js
@@ -5,19 +5,20 @@ module.exports = function(grunt){
 	grunt.registerMultiTask('hug', 'Wrap client-side files in anonymous functions, and concatenate with dependency solving', function(){
 		var complete = this.async(),
 			options = this.data,
-			gruntConfig = grunt.config.get(),
-			dest = grunt.template.process(this.data.dest, gruntConfig);
-			
-		console.log(dest);
-		options.src = grunt.file.expand({filter: "isFile"}, this.data.src);
+			dest = options.dest,
+			gruntConfig = grunt.config.data;
+		
+		options.src = grunt.file.expand({filter: "isFile"}, this.filesSrc);
 		options.exportedVariable = options.exportedVariable && grunt.template.process(options.exportedVariable, gruntConfig);
-		options.header = options.header? grunt.file.expand({filter: "isFile"}, options.header) : [];
-		options.path = options.path? grunt.file.expand({filter: "isDirectory"}, options.path) : [];
+		options.header = options.header? grunt.file.expandFiles(options.header) : [];
+		options.path = options.path? grunt.file.expand({filter: "isDirectory"},options.path) : [];
 		options.exports = options.exports &&  grunt.template.process(options.exports, gruntConfig);
 		options.moduleVariableName = options.moduleVariableName && grunt.template.process(options.moduleVariableName, gruntConfig);
 		options.exportsVariableName = options.exportsVariableName && grunt.template.process(options.exportsVariableName, gruntConfig);
 		options.requireFunctionName = options.requireFunctionName && grunt.template.process(options.requireFunctionName, gruntConfig);
+		
 		hug(options, function(content){
+			console.log(dest);
 			if(content instanceof Error){
 				grunt.log.error(content);
 				complete(false);
